8b88347dd84b01740bd72e02bd4d27b028d7ea47
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-04 21:22:51 -0500

    Feat: Attempt to connect with to MongoDB in typescript

---
+++ src/models/item.ts
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb";
+
+export default class Item {
+  constructor(
+    public title: string,
+    public description: string,
+    public seller: ObjectId,
+    public category: string,
+    public subcategory: string,
+    public price: number,
+    public timestamp: Date,
+    public photoFilenames: string[],
+    public ifSold: boolean,
+    public id?: ObjectId
+  ) {}
+}
