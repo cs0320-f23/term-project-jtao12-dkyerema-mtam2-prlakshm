66cd7b74844b058bec783b0162160fa5916a2687
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-05 22:09:51 -0500

    Feat: restart with Firebase

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Additional configuration options
-  // For example, adding TypeScript support
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-  },
 })
