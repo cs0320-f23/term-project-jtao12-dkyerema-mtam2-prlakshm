836f6a0662735633ee90500fb4e0ac9c4adafc3a
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-06 11:26:37 -0500

    Feat: print items into App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,14 @@ import { getAllItems } from "../mongo/test-read-types";
  */
 function App() {
 
-  getAllItems();
 
   return (
     <div className="App">
       <p className="App-header">
         <h1>Artist's Corner Pvd</h1>
       </p>
+
+      <p>{JSON.stringify(getAllItems())}</p>
       
     </div>
   );
