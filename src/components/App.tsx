44b35e39a50eb975f296a08203f813b59f5d4190
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-07 15:07:37 -0500

    Feat: working connection to mongodb app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,29 @@
-import { getAllItems } from "../mongo/test-read-types";
+//import { printAll } from "../mongo/test-read-java";
+import { useEffect, useState } from "react";
+import { ItemTuple, getAllItems } from "../mongo/test-read-types";
 /**
  * This is the highest level component!
  */
 function App() {
 
+  const [items, setItems] = useState<ItemTuple>([[],[]]);
 
+  useEffect(() => {
+    getAllItems()
+      .then(([masterItems, soldItems]) => {
+        console.log("Master Items:", masterItems);
+        console.log("Sold Items:", soldItems);
+        setItems([masterItems || [], soldItems || []]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []); 
   return (
     <div className="App">
       <p className="App-header">
         <h1>Artist's Corner Pvd</h1>
       </p>
-
-      <p>{JSON.stringify(getAllItems())}</p>
       
     </div>
   );
--- src/mongo/test-read-types.ts
+++ src/mongo/test-read-types.ts
@@ -1,22 +1,37 @@
 // test-read-types.ts
-import { MongoClient, Collection } from "mongodb";
+import {
+  Stitch,
+  RemoteMongoClient,
+  UserApiKeyCredential,
+  RemoteMongoCollection,
+} from "mongodb-stitch-browser-sdk";
 import Item from "../models/item";
 
 export type ItemTuple = [Item[], Item[]] | [];
 
+  const appId = "artists_corner_0-lcspi";
+  const client = Stitch.initializeDefaultAppClient(appId);
+  const mongodb = client.getServiceClient(
+    RemoteMongoClient.factory,
+    "mongodb-atlas"
+  );
 export async function getAllItems(): Promise<ItemTuple> {
-  const uri = "mongodb+srv://prlakshm:mKJocRtmhE6samGK@cs32term.vhg1g58.mongodb.net/";
-  const client = new MongoClient(uri);
 
   try {
-    await client.connect();
-    const db = client.db("artists_corner_pvd");
+    await client.auth.loginWithCredential(
+      new UserApiKeyCredential(
+        "iHXM2LKmwUIPYiBQqQvNOAnAm9QRTV4Qma04bxVp0c4rsszVNTpedtz2j9KIzkYN"
+      )
+    );
+    const db = mongodb.db("artists_corner_pvd");
 
-    const masterItemsCollection: Collection<Item> = db.collection("master_items");
+    const masterItemsCollection: RemoteMongoCollection<Item> =
+      db.collection("master_items");
     const masterItemsCursor = masterItemsCollection.find();
     const master_Items: Item[] = await masterItemsCursor.toArray();
 
-    const soldItemsCollection: Collection<Item> = db.collection("sold_items");
+    const soldItemsCollection: RemoteMongoCollection<Item> =
+      db.collection("sold_items");
     const soldItemsCursor = soldItemsCollection.find();
     const sold_Items: Item[] = await soldItemsCursor.toArray();
 
@@ -25,13 +40,5 @@ export async function getAllItems(): Promise<ItemTuple> {
     console.error("Error fetching items:", error);
     return [];
   } finally {
-    await client.close();
   }
 }
-
-getAllItems().then(([masterItems, soldItems]) => {
-  console.log("Master Items:", masterItems);
-  console.log("Sold Items:", soldItems);
-}).catch((error) => {
-  console.error("Error:", error);
-});
