c58b3da26b0915307547644cafd9794a4b4e6ff1
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-07 21:27:41 -0500

    Feat: first round of async functions finished
    Completed functions for retrieving from category, subcategory, search string

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
+//App.tsx
 import { useEffect, useState } from "react";
-import { ItemTuple, getAllItems } from "../mongo/test-read-types";
+import {
+  ItemTuple,
+  searchItems,
+  initializeStitchClient,
+} from "../mongo/Mongo-Functions";
+
 /**
  * This is the highest level component!
  */
 function App() {
   const [items, setItems] = useState<ItemTuple>([[], []]);
 
+  initializeStitchClient();
+
   useEffect(() => {
-    getAllItems()
+    searchItems("jacket")
       .then(([masterItems, soldItems]) => {
         setItems([masterItems || [], soldItems || []]);
       })
@@ -18,9 +26,33 @@ function App() {
   return (
     <div className="App">
       <h1>Artist's Corner Pvd</h1>
-      <p>{JSON.stringify(items)}</p>
+      
+      {/*formatted as bullet point list to make it easier to check items seperately*/}
+      <div>
+        <h2>Master Items</h2>
+        <ul>
+          {items[0]?.map((item, index) => (
+            <li key={index}>
+              Item: {JSON.stringify(item)}
+            </li>
+          ))}
+        </ul>
+      </div>
+  
+      {/*formatted as bullet point list to make it easier to check items seperately*/}
+      <div>
+        <h2>Sold Items</h2>
+        <ul>
+          {items[1]?.map((item, index) => (
+            <li key={index}>
+              Item: {JSON.stringify(item)}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
