2810291a41f2d5ced42fb550dab5fefaeab0120b
prlakshm <pranavi_lakshminarayanan@brown.edu>
2023-12-07 19:18:49 -0500

    Fix: connection to MongoDB fully debugged

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,24 @@
-//import { printAll } from "../mongo/test-read-java";
 import { useEffect, useState } from "react";
 import { ItemTuple, getAllItems } from "../mongo/test-read-types";
 /**
  * This is the highest level component!
  */
 function App() {
-
-  const [items, setItems] = useState<ItemTuple>([[],[]]);
+  const [items, setItems] = useState<ItemTuple>([[], []]);
 
   useEffect(() => {
     getAllItems()
       .then(([masterItems, soldItems]) => {
-        console.log("Master Items:", masterItems);
-        console.log("Sold Items:", soldItems);
         setItems([masterItems || [], soldItems || []]);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
-  }, []); 
+  }, []);
   return (
     <div className="App">
-      <p className="App-header">
-        <h1>Artist's Corner Pvd</h1>
-      </p>
-      
+      <h1>Artist's Corner Pvd</h1>
+      <p>{JSON.stringify(items)}</p>
     </div>
   );
 }
