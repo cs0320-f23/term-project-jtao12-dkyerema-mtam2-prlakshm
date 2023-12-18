c0987a722aba444348708f98f0d536630d98fc5d
jtao25 <jeffrey_tao@brown.edu>
2023-12-18 12:11:44 -0500

    feat: upcoming events added below slideshow on home

--- artists_corner_0/frontend/web.mql.todo/src/components/HomePage.jsx
+++ artists_corner_0/frontend/web.mql.todo/src/components/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Slideshow from "./Slideshow";
+import crab from "../assets/crab-eboard.jpg";
+import magnet from "../assets/magnet-workshop.jpg";
+import model from "../assets/model-call.jpg";
 
 function HomePage() {
   return (
@@ -7,6 +10,21 @@ function HomePage() {
       <div>
         <Slideshow />
       </div>
+
+      <section className="team">
+        <h2>Upcoming Events</h2>
+        <div className="team-members">
+          <div className="team-member">
+            <img src={crab} alt="Create @ Brown E-board Application" />
+          </div>
+          <div className="team-member">
+            <img src={magnet} alt="M" />
+          </div>
+          <div className="team-member">
+            <img src={model} alt="Fashion @ Brown Model Call" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
