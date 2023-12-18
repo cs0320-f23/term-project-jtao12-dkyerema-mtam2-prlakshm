ffd9e33cd4ed4ad3b93fa8804114aaa62c50ccd3
dkyerema <dorinda_kyeremateng@brown.edu>
2023-12-18 17:25:46 -0600

    feat: filter buttons and separation

--- artists_corner_0/frontend/web.mql.todo/src/components/CategoryPage.jsx
+++ artists_corner_0/frontend/web.mql.todo/src/components/CategoryPage.jsx
@@ -3,21 +3,28 @@ import { useParams } from "react-router-dom";
 import {
   getItemsByCategory,
   getItemsBySubcategory,
+  sortPriceLowToHigh,
+  sortByPriceHighToLow,
+  sortMostToLeastRecent,
+  sortLeastToMostRecent
 } from "../mongo/Mongo-Functions";
 import ItemComponent from "./ItemComponent";
 import "../styles/category.css";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
-  const [items, setItems] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]);
   const [subcategories, setSubcategories] = useState(new Set());
   const [selectedSubcategory, setSelectedSubcategory] = useState("");
+  const [selectedFilter, setSelectedFilter] = useState([]);
+  // const [originalItems, setOriginalItems] = useState([]); // Represents the original unsorted items.
+
 
   const fetchItems = async () => {
     try {
       const [masterItems, soldItems] = await getItemsByCategory(categoryName);
       const combinedItems = [...masterItems, ...soldItems];
-      setItems(combinedItems);
+      setCurrentItems(combinedItems);
 
       // Extract and set subcategories
       const subcategoriesSet = new Set(
@@ -33,15 +40,66 @@ const CategoryPage = () => {
     fetchItems();
   }, [categoryName]);
 
+  useEffect(() => {
+    if (selectedFilter) {
+      console.log("selected filter:" + selectedFilter)
+      console.log("current items:" + currentItems)
+
+      const sortedItems = sortItemsByOption(selectedFilter, currentItems);
+      setCurrentItems(sortedItems);
+    } else {
+      setCurrentItems(currentItems);
+    }
+  }, [selectedFilter, currentItems]);
+
   const handleSubcategoryClick = async (subcategory) => {
     setSelectedSubcategory(subcategory);
     if (subcategory) {
       const [masterItems, soldItems] = await getItemsBySubcategory(subcategory);
-      setItems([...masterItems, ...soldItems]);
+      setCurrentItems([...masterItems, ...soldItems]);
     } else {
       fetchItems(); // Reset to all items in the category
     }
   };
+
+const handleSort = async () => {
+  if (selectedFilter) {
+    const sortedItems = sortItemsByOption(selectedFilter, currentItems);
+    setCurrentItems(sortedItems);
+    
+  } else {
+    fetchItems();
+  }
+};
+  
+  const sortItemsByOption = (filter, items) => {
+    switch (filter) {
+      case "lowToHigh":
+        return [
+          sortPriceLowToHigh(items[0]), // Sort master items
+          sortPriceLowToHigh(items[1]), // Sort sold items
+        ];
+      case "highToLow":
+        return [
+          sortByPriceHighToLow(items[0]), // Sort master items
+          sortByPriceHighToLow(items[1]), // Sort sold items
+        ];
+      case "mostRecent":
+        return [
+          sortMostToLeastRecent(items[0]), // Sort master items
+          sortMostToLeastRecent(items[1]), // Sort sold items
+        ];
+      case "leastRecent":
+        return [
+          sortLeastToMostRecent(items[0]), // Sort master items
+          sortLeastToMostRecent(items[1]), // Sort sold items
+        ];
+      default:
+        return items;
+    }
+  };
+  
+  
 
   return (
     <div className="category-page-container">
@@ -66,9 +124,36 @@ const CategoryPage = () => {
             Reset Filter
           </button>
         )}
+
+        {[
+          { label: "low to high", value: "lowToHigh" },
+          { label: "high to low", value: "highToLow" },
+          { label: "most recent", value: "mostRecent" },
+          { label: "least recent", value: "leastRecent" },
+        ].map((filter) => (
+          <button
+            key={filter.value}
+            className={`filter-tag ${
+              selectedFilter === filter.value ? "selected" : ""
+            }`}
+            onClick={() => setSelectedFilter(filter.value)}
+            // onClick={() => handleSort()}
+            >
+            {filter.label}
+          </button>
+        ))}
+
+        {selectedSubcategory && (
+          <button
+            className="reset-filter"
+            onClick={() => handleSubcategoryClick("")}
+          >
+            Sort By
+          </button>
+        )}
       </div>
       <div className="items-grid">
-        {items.map((item) => (
+        {currentItems.map((item) => (
           <ItemComponent key={item._id} item={item} />
         ))}
       </div>
